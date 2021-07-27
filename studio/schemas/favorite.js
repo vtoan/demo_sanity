8b780e8afdb6a0b959b61bc7798a9eb03cf42484
Toan Dang <dtoan2805@gmail.com>
2021-07-28 00:28:17 +0700

    init project

---
+++ studio/schemas/favorite.js
@@ -0,0 +1,25 @@
+export default {
+  name: "favorite",
+  title: "Favorite",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      description: 'Please use "Firstname Lastname" format',
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 100,
+      },
+    },
+  ],
+  preview: {
+    select: { title: "name" },
+  },
+};
